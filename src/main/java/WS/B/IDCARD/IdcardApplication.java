3c8fa2e3c675c4c3d4d2ba1e06f5c6a3a2891aa8
Zahran Rafif <zahranlumia@gmail.com>
2022-11-15 06:28:27 +0700

    First Commit

---
+++ src/main/java/WS/B/IDCARD/IdcardApplication.java
@@ -0,0 +1,13 @@
+package WS.B.IDCARD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdcardApplication.class, args);
+	}
+
+}
